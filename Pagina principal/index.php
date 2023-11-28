a0818c5be4b5f7c42041d192fb25a759df4aded5
Algorithmus Vita <150991716+AlgorithmusVita@users.noreply.github.com>
2023-11-27 22:24:14 -0300

    Update index.php

--- Pagina principal/index.php
+++ Pagina principal/index.php
@@ -10,31 +10,33 @@
     <script src='main.js'></script>
 </head>
 <body>
-    <div id="barraMenu">  
-        <div class="login">
-        <p class="logintext">Entrar</p>
-        </div>
-        <div class="Inicio" width>   
-            </div>
-        <div class="Serviços" >   
-            <p >Serviços</p>
-            
-            </div>
-        <div class="Contato">   
-            
-            </div>
-        <div class="SobreNos">   
-            
-            </div>
-           
-    </div>
-    <div id="corpoPagina">
+ 
+        <header>
+        <h1 id="cima">Algorithmus Vita</h1>
+        <nav>
+         <a href="# "class="menu" id="home">Home</a>
+         <a href="#" class="menu" id="sobrenos">Sobre Nós</a>
+         <a href="#" class="menu" id="curiosidades">Curiosidades</a>
+         <a href="#" class="menu" id="faleconosco">Fale conosco</a>
+        </nav>
+    </header>
+
+    <div id="corpo">
+        <p class="content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis recusandae nam maiores explicabo, quas consectetur repellat accusantium nemo provident fugit. Esse, deserunt! Nesciunt sint sequi ipsam tenetur quibusdam harum accusantium. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum non eligendi necessitatibus sint quidem nam. Ducimus molestias temporibus cum illo non animi culpa aspernatur architecto voluptatibus, labore inventore itaque sequi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium earum numquam excepturi beatae nesciunt molestias recusandae odit, nemo labore ea fugit tempora <i><strong>preto</strong></i> est aperiam autem voluptatem. Ratione, quia dolore?</p>
+        <p class="content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis obcaecati quod repellat similique esse non fugit rem odit facere. Totam dicta blanditiis illum dignissimos eaque ullam praesentium sed at debitis? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente voluptates suscipit alias, nesciunt explicabo laudantium eaque consequatur eos quisquam fugiat optio omnis ad? Ipsa enim aperiam fugit iste, ex dolorum? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet ipsa eveniet delectus accusantium! Sapiente, deserunt voluptate iure adipisci alias quisquam accusamus quibusdam. Sequi soluta voluptatem dignissimos voluptatum odit, molestias quis?</p>
+
+        <img src="imagens/teste.png" alt="Hehe" id="teste">
 
 
     </div>
+
+
+
+
+
     <php>
-    
 
     </php>
+    <footer></footer>
 </body>
 </html>
