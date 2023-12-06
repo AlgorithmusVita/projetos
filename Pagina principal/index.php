a97ace4d4baf08f849d7bb5316a11f777f69d1d2
Brendon Alexander <alexander.brendon2002@gmail.com>
2023-12-06 16:54:30 -0300

    Header test

--- Pagina principal/index.php
+++ Pagina principal/index.php
@@ -11,29 +11,31 @@
 <body>
  
         <header> 
-       <img src="imagens/1logo.png" alt="AV LOGO" id="logo">
+        <!-- Mover o logo para cima ao lado do logo -->
+        <img src="imagens/avlogo.png" alt="AV LOGO" id="logo">
         <h1 id="cima">Algorithmus Vita</h1>
         <nav>
-         <a href="#home" class="menu"> Home</a>
-         <a href="#sobrenos" class="menu"> Sobre Nós</a>
-         <a href="#curiosidades" class="menu"> Curiosidades</a>
-         <a href="#faleconosco" class="menu"> Fale conosco</a>
+         <a href="#home" class="menu"> Home</a> 
+         <a href="#sobrenos" class="menu"> Sobre Nós</a> 
+         <a href="#curiosidades" class="menu"> Curiosidades</a> 
+         <a href="#faleconosco" class="menu"> Fale conosco</a> 
         </nav>
     </header>
 
+     <!-- Tentar intercalar com secções com o fundo escuro -->
     <div id="home">
         <h2>Lorem ipsum</h2>
         <p class="content">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil, deserunt, expedita iure quaerat, maiores consequuntur nulla praesentium sed ea excepturi vero. Quo velit perspiciatis repellat inventore ipsum nihil soluta quod! Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis recusandae nam maiores explicabo, quas consectetur repellat accusantium nemo provident fugit. Esse, deserunt! Nesciunt sint sequi ipsam tenetur quibusdam harum accusantium. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum non eligendi necessitatibus sint quidem nam. Ducimus molestias temporibus cum illo non animi culpa aspernatur architecto voluptatibus, labore inventore itaque sequi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium earum numquam excepturi beatae nesciunt molestias recusandae odit, nemo labore ea fugit tempora <i><strong>preto</strong></i> est aperiam autem voluptatem. Ratione, quia dolore?</p>
         <p class="content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis obcaecati quod repellat similique esse non fugit rem odit facere. Totam dicta blanditiis illum dignissimos eaque ullam praesentium sed at debitis? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente voluptates suscipit alias, nesciunt explicabo laudantium eaque consequatur eos quisquam fugiat optio omnis ad? Ipsa enim aperiam fugit iste, ex dolorum? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet ipsa eveniet delectus accusantium! Sapiente, deserunt voluptate iure adipisci alias quisquam accusamus quibusdam. Sequi soluta voluptatem dignissimos voluptatum odit, molestias quis?</p>
         
     </div>
-    <img src="imagens/teste.png" alt="Hehe" id="teste">
+    <div><img src="imagens/teste.png" alt="Hehe" id="teste"></div>
     <div>
         <h2 id="sobrenos">Sobre nós</h2>
         <p class="content">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem eligendi laborum maiores nisi soluta at. Veritatis, tempore ducimus eius minima facere magni corrupti consequatur corporis veniam fuga perspiciatis, sit cum. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates nobis culpa quisquam veniam beatae officiis iusto optio aliquid? Soluta eveniet eos inventore natus, cum iure beatae ea animi libero incidunt.</p>
         <p class="content">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis corrupti voluptate vel a. Voluptas dignissimos aliquid velit, fugiat quis quae veritatis consequatur, laboriosam sapiente incidunt necessitatibus illo modi, rem soluta. Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero mollitia suscipit aperiam vel maiores eius aut tenetur illum quisquam, recusandae aliquid soluta officiis. Deserunt illo doloribus reprehenderit! In, necessitatibus quae!</p>
     </div>
-
+    <!-- RESOLVER DEPOIS: As divs não seguem a mesma configuração quando o tamanho da tela é reduzido -->
     <div>
     <h2 id="curiosidades">Curiosidades</h2>
         <p class="content">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem eligendi laborum maiores nisi soluta at. Veritatis, tempore ducimus eius minima facere magni corrupti consequatur corporis veniam fuga perspiciatis, sit cum. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates nobis culpa quisquam veniam beatae officiis iusto optio aliquid? Soluta eveniet eos inventore natus, cum iure beatae ea animi libero incidunt.</p>
@@ -45,7 +47,7 @@
         <p class="content">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem eligendi laborum maiores nisi soluta at. Veritatis, tempore ducimus eius minima facere magni corrupti consequatur corporis veniam fuga perspiciatis, sit cum. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates nobis culpa quisquam veniam beatae officiis iusto optio aliquid? Soluta eveniet eos inventore natus, cum iure beatae ea animi libero incidunt.</p>
         <p class="content">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis corrupti voluptate vel a. Voluptas dignissimos aliquid velit, fugiat quis quae veritatis consequatur, laboriosam sapiente incidunt necessitatibus illo modi, rem soluta. Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero mollitia suscipit aperiam vel maiores eius aut tenetur illum quisquam, recusandae aliquid soluta officiis. Deserunt illo doloribus reprehenderit! In, necessitatibus quae!</p>
     </div>
-    <div class="pblack">
+    <div>
         <h2 id="faleconosco">Entre em contato</h2>
             <p class="content" >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem eligendi laborum maiores nisi soluta at. Veritatis, tempore ducimus eius minima facere magni corrupti consequatur corporis veniam fuga perspiciatis, sit cum. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates nobis culpa quisquam veniam beatae officiis iusto optio aliquid? Soluta eveniet eos inventore natus, cum iure beatae ea animi libero incidunt.</p>
     </div>
